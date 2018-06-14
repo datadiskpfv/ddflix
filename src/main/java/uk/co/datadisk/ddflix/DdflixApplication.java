05bd7041d110489c9bd62e80c65abce42ca1d649
pvalle <paul.valle@datadisk.co.uk>
2018-06-14 13:33:05 +0100

    Initial commit

---
+++ src/main/java/uk/co/datadisk/ddflix/DdflixApplication.java
@@ -0,0 +1,12 @@
+package uk.co.datadisk.ddflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdflixApplication.class, args);
+	}
+}
